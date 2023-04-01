bbb97dc7f48ac92dfd62b9458f3b016f734f9133
Edward Galantay <31413546+karlmtr@users.noreply.github.com>
2023-04-01 23:09:10 +0200

    update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 import matplotlib.pyplot as plt
 from wa_analyser import Discussion
 import pandas as pd
-import asyncio
-
+import numpy as np
+import datetime
 import concurrent.futures
 
 
@@ -15,60 +15,184 @@ def get_files(root_dir: str) -> list[pathlib.Path]:
     return paths
 
 
+def who_creates_new_discussion(df):
+    """
+    df has to be general
+    """
+    users = df["name"].value_counts().index.to_list()
+    data = {user: [] for user in users}
+    time = np.arange(1, 50)
+    for hour in time:
+        delta = datetime.timedelta(days=0, hours=int(hour))
+        temp_data = []
+        for i in range(1, len(df["date"])):
+            if df["date"][i] - df["date"][i - 1] > delta:
+                temp_data.append(df["name"][i])
+        for user in users:
+            data[user].append(temp_data.count(user))
+    return time, data
+
+
+def best_hour(df: pd.DataFrame):
+    return (df["date"].dt.hour + df["date"].dt.minute / 60).mean()
+
+
+# def get_reccurence_of_words(df: pd.DataFrame):
+#     text_col = df["content"]
+#     df["words"] = text_col.apply(tokenize_text)
+#     exploded_df = df.explode("words")
+#     word_counts = exploded_df["words"].value_counts()
+#     print(word_counts)
+
+
+def count_medias(df: pd.DataFrame):
+    try:
+        nb = df["content"].value_counts()["<Media omitted>\n"]
+    except KeyError:
+        return 0
+    else:
+        return nb
+
+
 def get_infos(df: pd.DataFrame) -> dict:
     """
     Nb messages par user + pourcentage
     Meilleure heure dans la journée pour chaque user
     """
     infos = {}
+    infos["Nb messages"] = len(df)
+    infos["Meilleure heure (h)"] = round(best_hour(df), 1)
+    infos["Caractère/message"] = round(caracter_per_messages(df), 1)
+    infos["Nbre médias (audios compris)"] = count_medias(df)
+    infos["Moy. heures entre deux messages"] = round(
+        np.mean(average_time_between_messages(df)), 1
+    )
+    return infos
+
+
+def caracter_per_messages(df: pd.DataFrame):
+    nb_caracters = df["content"].str.len().sum()
+    nb_message = len(df["content"])
+    return nb_caracters / nb_message
+
+
+def average_time_between_messages(df: pd.DataFrame):
+    diff = np.roll(df["date"], -1) - df["date"]
+    return diff[:-1].dt.total_seconds() / 3600
+
+
+def week_format(df: pd.DataFrame):
+    return (
+        df["date"].dt.day_of_week
+        + df["date"].dt.hour / 24
+        + df["date"].dt.minute / 1440
+    )
+
+
+def create_document(df: pd.DataFrame, specific_user=None):
     users = df["name"].value_counts().index.to_list()
-    nb_messages = {}
+    bigGroup = False
+    if len(users) > 9 : 
+       bigGroup = True 
     
-    for user in users:
-        nb_messages[user] = df[df["name"] == user].count()
-    
-    infos["nb_messages"] = nb_messages # dict
-
-    
-
-
-
-def create_document(df):
-    users = df["name"].value_counts().index.to_list()
+    print(len(users))
     short_users = [user.split(" ")[0] for user in users]
-
-    fig, (ax, ax1, ax2) = plt.subplots(nrows=3, ncols=1, figsize=(10, 7))
-    fig.suptitle(f"{' & '.join(short_users)}")
+    if not bigGroup : 
+        fig, (ax, ax1, ax4, ax2, ax3) = plt.subplots(nrows=5, ncols=1, figsize=(30,40)) 
+        fig.suptitle(f"{' & '.join(short_users)}")
+    else : 
+        fig, (ax, ax1, ax4, ax2, ax3) = plt.subplots(nrows=5, ncols=1, figsize=(30,40)) 
+        
 
     ax1.set(
-        title="Message distrib. over time",
+        title="Message distrib. / année",
         ylabel="# messages",
         xlabel="dates",
     )
     ax2.set(
-        title="Message distrib. over the day",
+        title="Message distrib. / jour",
         ylabel="# messages",
         xlabel="dates",
     )
-    for user in users:
-        filt_df = df[df["name"] == user]
+    ax3.set(
+        title="Nombre de 'relance' après X heures",
+        ylabel="# relances",
+        xlabel="Delta heure",
+    )
+    ax4.set(
+        title="Messages dist. / semaine ",
+        ylabel="# messages",
+        xlabel="Jours semaine",
+       
+    )
+    ax4.set_xticklabels(("","lundi", "mardi", "mercredi","jeudi","vendredi","samedi","dimanche"))
+
+    ax.axis(False)
+    MIN_Y, MAX_Y = ax.get_ylim()
+    MIN_Y, MAX_Y = MIN_Y * 0.9, MAX_Y * 0.9  # padding
+    MIN_X, MAX_X = ax.get_xlim()
+
+    if not bigGroup :
+        timedeltas, who_created = who_creates_new_discussion(df)
+        for i, user in enumerate(users):
+            filt_df = df[df["name"] == user]
+            ax1.hist(
+                filt_df["date"],
+                bins=100,
+                label=user,
+                alpha=0.7,
+            )
+            ax2.hist(filt_df["date"].dt.hour, bins=24, alpha=0.7, label=user)
+
+            ax3.plot(timedeltas, who_created[user], alpha=0.7, label=user)
+            ax4.hist(week_format(filt_df), bins=24 * 7, alpha=0.7, label=user)
+
+            ax.text(i / len(users), MAX_Y, user, va="center", ha="left", weight="bold")
+
+            infos = get_infos(filt_df)
+            for index, (key, value) in enumerate(infos.items()):
+                ax.text(
+                    i / len(users),
+                    MAX_Y * 0.9 - index * 0.05,
+                    f"{key} : {value}",
+                    weight="light",
+                )
+    else : 
         ax1.hist(
-            filt_df["date"],
+            df["date"],
             bins=100,
-            label=f"{user} ({len(filt_df)}  mess. )",
-            alpha=0.5,
+            alpha=0.7,
         )
-        ax2.hist(filt_df["date"].dt.hour, bins=24, alpha=0.5, label=user)
+        ax2.hist(df["date"].dt.hour, bins=24, alpha=0.7)
+
+        # ax3.plot(timedeltas, who_created[user], alpha=0.7)
+        ax4.hist(week_format(df), bins=24 * 7, alpha=0.7)
+        
+
+    # ax4.hist2d(df[df["name
+    ax.text(MIN_X, MAX_Y / 2, "General", weight="bold", va="center", ha="left")
+    gen_infos = get_infos(df)
+    for index, (key, value) in enumerate(gen_infos.items()):
+        ax.text(
+            MIN_X, MAX_Y / 2 * 0.8 - index * 0.05, f"{key} : {value}", weight="light"
+        )
+
+    if not bigGroup :
         ax1.legend()
         ax2.legend()
-
+        ax3.legend()
+        ax4.legend()
+    
     fig.tight_layout()
 
     ## checking if the results folder already exists
     if not os.path.exists("./results/"):
         os.mkdir("./results")
 
-    fig.savefig(f"./results/{'_'.join(short_users)}.pdf")
+    if not bigGroup:
+        fig.savefig(f"./results/{'_'.join(short_users)}.pdf")
+    else :
+        fig.savefig(f"./results/group_{users[0]}.pdf")
 
 
 def main(file):
@@ -79,8 +203,9 @@ def main(file):
 
 if __name__ == "__main__":
     files = get_files("data")
-    # with concurrent.futures.ThreadPoolExecutor() as executor :
-    #     results = [executor.submit(main(file)) for file in files]
-    for file in files:
-        main(file)
-        break
+    with concurrent.futures.ProcessPoolExecutor() as exec :
+        results = [exec.submit(main, file) for file in files]
+    # for file in files:
+        # print(file)
+    # main(pathlib.Path("data/test.txt"))
+        # break
--- wa_analyser/discussion.py
+++ wa_analyser/discussion.py
@@ -3,29 +3,55 @@ import re
 from datetime import datetime
 import pandas as pd
 
+import glob
+
 
 class Discussion:
     def __init__(self, path: pathlib.Path) -> None:
         with open(path, "r") as fp:
             self.content = fp.read()
+            self.content = re.sub('', '', self.content )
 
     def to_dataframe(self) -> pd.DataFrame:
-        reg_pat = r"\[?(\d{2}(?:\.|\/)\d{2}(?:\.|\/)\d{2,4},\s\d{2}:\d{2}(?:\:\d{2}\s(?:AM|PM))?)\]?\s(?:-\s)?((?:\w\s?)+):"
-        matches = re.findall(reg_pat, self.content)
+        # reg_pat = r"\[?(\d{2}(?:\.|\/)\d{2}(?:\.|\/)\d{2,4},\s\d{2}:\d{2}(?:\:\d{2}\s(?:AM|PM))?)\]?\s(?:-\s)?((?:\w\s?)+):\s"
+        # reg_pat = r"\[?(\d{2}(?:\.|\/)\d{2}(?:\.|\/)\d{2,4},?\s\d{2}:\d{2}(?:\:\d{2}\s?(?:AM|PM)?)?)\]?\s(?:-\s)?((?:\w\s?)+):\s"
+        reg_pat = r"\[?(\d{2}(?:\.|\/)\d{2}(?:\.|\/)\d{2,4},?\s\d{2}:\d{2}(?:\:\d{2}\s?(?:AM|PM)?)?)\]?\s(?:-\s)??((?:\+?\w\s?)+):\s"
+        # matches = re.findall(reg_pat, self.content)
         dates = []
         names = []
-        for date, name in matches:
-            if len(name) < 40:
+        content = []
+        oldEnd = 0
+        first = True
+        reg_matches = re.finditer(reg_pat, self.content) 
+        message = ""
+        match = None
+        for match in reg_matches :    
+            date = match[1]
+            name = match[2]
+            
+            start, end = match.span()
+            if len(name) < 40 :
+                if not first:
+                    message = self.content[oldEnd:start]
+                    content.append(message) 
                 try:
                     date_obj = datetime.strptime(date, r"%d/%m/%Y, %H:%M")
                 except ValueError:
-                    date_obj = datetime.strptime(date, r"%d.%m.%y, %I:%M:%S %p")
+                    try :
+                        date_obj = datetime.strptime(date, r"%d.%m.%y %H:%M:%S")
+                    except ValueError : 
+                        date_obj = datetime.str
+                        
                 dates.append(date_obj)
                 names.append(name)
-            else:
-                print(name)
+                
+            oldEnd = end
+            first = False  
 
-        df = pd.DataFrame({"date": dates, "name": names}).astype(
+        content.append(self.content[match.end():])
+        df = pd.DataFrame({"date": dates, "name": names, "content" : content}).astype(
             {"date": "datetime64[ns]", "name": "category"}
         )
         return df
+
+    
